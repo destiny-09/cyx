30e5c0ef79b31d767e3610e8f2b9d0365c2e0cf2
destiny-09 <Cyx460513727>
2019-11-13 17:54:12 +0800

    add dubbo logback

--- logbackstudy/src/main/java/TestLogBack.java
+++ logbackstudy/src/main/java/TestLogBack.java
@@ -11,6 +11,6 @@ public class TestLogBack {
     private static final Logger logger = LoggerFactory.getLogger(TestLogBack.class);
 
     public static void main(String[] args) {
-        logger.info("===================++==============");
+        logger.info("===================++===============");
     }
 }
